2f45a3967d804fa2f55402e3ff74b53cf168f3e4
cosmodesant49 <emilmaldybaev49@gmail.com>
2023-08-05 18:42:01 +0600

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Вызов метода с различными входными данными
+        System.out.println(ageAndTemperature(25, 15));
+        System.out.println(ageAndTemperature(18, 20));
+        System.out.println(ageAndTemperature(30, 35));
+        System.out.println(ageAndTemperature(50, -5));
+        System.out.println(ageAndTemperature(22, 32));
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static String ageAndTemperature(int age, int temperature) {
+        if ((age > 20 && age <= 45) && (temperature >= -20 && temperature <= 30)) {
+            return "Можно идти гулять";
+        } else if (age < 20 && (temperature >= 0 && temperature <= 28)) {
+            return "Можно идти гулять";
+        } else if (age > 45 && (temperature >= -10 && temperature <= 25)) {
+            return "Можно идти гулять";
+        } else {
+            return "Оставайтесь дома";
         }
     }
 }
